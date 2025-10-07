04c5730af30aa4cb963940ab4838509e3a8efbed
Ben Teichman <ben@voiceflow.com>
2025-10-07 18:26:09 -0400

    fix: export DialogContext (#9)

--- libs/web/src/main.ts
+++ libs/web/src/main.ts
@@ -1,5 +1,6 @@
 export * from './components/box/box.component';
 export * from './components/dialog/dialog.component';
+export * from './components/dialog/dialog.context';
 export * from './styles/theme.css';
 export * from './utils/resource.util';
 export * from './utils/template.util';
