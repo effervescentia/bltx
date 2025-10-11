46703cc557cf1e3c16762796e0e09f50e9777505
Ben Teichman <ben@voiceflow.com>
2025-10-11 02:06:42 -0400

    refactor: use drizzle type helpers

--- libs/db/src/utils/crud.util.ts
+++ libs/db/src/utils/crud.util.ts
@@ -1,24 +1,24 @@
 import type { AnyRecord } from '@bltx/core';
-import type { SQL } from 'drizzle-orm';
+import type { InferInsertModel, InferSelectModel, SQL } from 'drizzle-orm';
 import type { PgTable } from 'drizzle-orm/pg-core';
 import type { Database } from '../db.types';
 
 export const insertOne = async <Schema extends AnyRecord, Table extends PgTable>(
   db: Database<Schema>,
   table: Table,
-  values: Table['$inferInsert'],
+  values: InferInsertModel<Table>,
 ) => {
   const results = await db.insert(table).values(values).returning();
-  return results[0] as Table['$inferSelect'];
+  return results[0] as InferSelectModel<Table>;
 };
 
 export const updateOne = async <Schema extends AnyRecord, Table extends PgTable>(
   db: Database<Schema>,
   table: Table,
   where: SQL,
-  values: Partial<Table['$inferInsert']>,
+  values: Partial<InferInsertModel<Table>>,
 ) => {
-  const results = (await db.update(table).set(values).where(where).returning()) as Table['$inferSelect'][];
+  const results = (await db.update(table).set(values).where(where).returning()) as InferSelectModel<Table>[];
   if (!results.length) throw new Error(`Failed to update row from ${table._.name} table`);
   return results[0];
 };
