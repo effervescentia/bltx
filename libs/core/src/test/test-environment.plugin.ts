d7697baa406ccda938bf0a7741d041c77ddc1697
Ben Teichman <ben@voiceflow.com>
2025-10-21 12:34:33 -0400

    feat: use test env vars (#27)

--- libs/core/src/test/test-environment.plugin.ts
+++ libs/core/src/test/test-environment.plugin.ts
@@ -3,4 +3,4 @@ import { ENVIRONMENT_PLUGIN } from '../core.const';
 import type { AnyRecord } from '../core.types';
 
 export const TestEnvironmentPluginFactory = <Env extends AnyRecord>(env: Env) =>
-  new Elysia({ name: ENVIRONMENT_PLUGIN }).use((app) => app.decorate({ env }));
+  new Elysia({ name: ENVIRONMENT_PLUGIN }).use((app) => app.decorate({ env: () => env }));
