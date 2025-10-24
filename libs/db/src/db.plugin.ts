6b17806163b132dbd80c9a1ee3cb20212b67ca2c
Ben Teichman <ben@voiceflow.com>
2025-10-24 17:09:15 -0400

    fix: allow type inference using db plugin

--- libs/db/src/db.plugin.ts
+++ libs/db/src/db.plugin.ts
@@ -1,4 +1,4 @@
-import type { AnyRecord } from '@bltx/core';
+import type { AnyRecord, createEnvironmentPlugin } from '@bltx/core';
 import { PGlite, type PGliteOptions } from '@electric-sql/pglite';
 import { cube } from '@electric-sql/pglite/contrib/cube';
 import { earthdistance } from '@electric-sql/pglite/contrib/earthdistance';
@@ -7,21 +7,13 @@ import { vector } from '@electric-sql/pglite/vector';
 import type { SQL } from 'bun';
 import { drizzle as drizzleBunSQL } from 'drizzle-orm/bun-sql';
 import { drizzle as drizzlePGlite } from 'drizzle-orm/pglite';
-import Elysia, { type SingletonBase } from 'elysia';
+import Elysia, { type TSchema } from 'elysia';
 
 import { match } from 'ts-pattern';
 import { DATABASE_PLUGIN } from './db.const';
 import type { DatabaseLike } from './db.types';
 
-type EnvironmentPluginInstance<Env> = Elysia<
-  any,
-  SingletonBase & { decorator: { env: () => Env } },
-  any,
-  any,
-  any,
-  any,
-  any
->;
+type EnvironmentPluginInstance<Env> = ReturnType<typeof createEnvironmentPlugin<TSchema & { static: Env }>>;
 
 export type DatabaseOptions<Schema, Env> =
   | {
@@ -46,7 +38,7 @@ export const createPGliteDatabaseClient = (options?: PGliteOptions) =>
 export const createDatabasePlugin = <Schema extends AnyRecord, Env extends AnyRecord>(
   options: DatabaseOptions<Schema, Env>,
 ) =>
-  new Elysia({ name: DATABASE_PLUGIN }).use(options.env).use((app) => {
+  new Elysia({ name: DATABASE_PLUGIN }).use((app) => {
     if (import.meta.env.NODE_ENV === 'test') {
       return app.decorate({
         db: (): DatabaseLike<Schema> => {
@@ -55,13 +47,11 @@ export const createDatabasePlugin = <Schema extends AnyRecord, Env extends AnyRe
       });
     }
 
+    const env = options.env.decorator.env();
+
     const db = match(options)
-      .with({ type: 'bun-sql' }, ({ schema, factory }) =>
-        drizzleBunSQL({ schema, client: factory(app.decorator.env()) }),
-      )
-      .with({ type: 'pglite' }, ({ schema, factory }) =>
-        drizzlePGlite({ schema, client: factory(app.decorator.env()) }),
-      )
+      .with({ type: 'bun-sql' }, ({ schema, factory }) => drizzleBunSQL({ schema, client: factory(env) }))
+      .with({ type: 'pglite' }, ({ schema, factory }) => drizzlePGlite({ schema, client: factory(env) }))
       .exhaustive();
 
     return app.decorate({ db: () => db });
