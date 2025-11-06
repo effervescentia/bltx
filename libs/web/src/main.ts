dc4aacbc90bf0bbbc05e9c77c2ebff22fa1e056d
Ben Teichman <ben@voiceflow.com>
2025-11-06 02:15:09 -0500

    fix: avoid infinite loop loading resources

--- libs/web/src/main.ts
+++ libs/web/src/main.ts
@@ -1,6 +1,7 @@
 export * from './components/box/box.component';
 export * from './components/dialog/dialog.component';
 export * from './components/dialog/dialog.context';
+export * from './hooks/use-const.hook';
 export * from './styles/theme.css';
 export * from './utils/resource.util';
 export * from './utils/template.util';
--- libs/web/src/utils/resource.util.ts
+++ libs/web/src/utils/resource.util.ts
@@ -2,6 +2,7 @@ import type { Treaty } from '@elysiajs/eden';
 import { atom, useAtom } from 'jotai';
 import { atomFamily, atomWithRefresh, loadable } from 'jotai/utils';
 import { useEffect } from 'react';
+import { useConst } from '../hooks/use-const.hook';
 
 export type ResourceValue<T> =
   | { state: 'loading' }
@@ -17,7 +18,8 @@ export const resource = <T>(fetch: (resourceID: string) => Promise<Treaty.Treaty
   const family = atomFamily((resourceID: string) => loadable(atom(() => fetch(resourceID))));
 
   return (resourceID: string, options?: ResourceOptions): ResourceValue<T> & { refresh: () => void } => {
-    const [value, refresh] = useAtom(atomWithRefresh((get) => get(family(resourceID))));
+    const refreshAtom = useConst(() => atomWithRefresh((get) => get(family(resourceID))));
+    const [value, refresh] = useAtom(refreshAtom);
     const notFound = value.state === 'hasData' && !value.data.data;
 
     useEffect(() => {
@@ -46,7 +48,8 @@ export const resources = <T>(fetch: () => Promise<Treaty.TreatyResponse<{ 200: T
   const loadableAtom = loadable(atom(fetch));
 
   return (): ResourceValue<T> & { refresh: () => void } => {
-    const [value, refresh] = useAtom(atomWithRefresh((get) => get(loadableAtom)));
+    const refreshAtom = useConst(() => atomWithRefresh((get) => get(loadableAtom)));
+    const [value, refresh] = useAtom(refreshAtom);
 
     if (value.state === 'hasData') {
       if (value.data.error) {
