cc06ddb1de3a6735119a5f501e4f845e318add4e
Ben Teichman <ben@voiceflow.com>
2026-02-17 02:18:23 -0500

    fix: separate drizzle-kit from main db exports (#46)

--- libs/db/src/main.ts
+++ libs/db/src/main.ts
@@ -1,6 +1,5 @@
 export * from './db.plugin';
 export * from './db.types';
-export * from './migrate/db-push';
 export * from './utils/column.util';
 export * from './utils/crud.util';
 export * from './utils/query.util';
